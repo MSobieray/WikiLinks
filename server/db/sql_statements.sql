a116323d449d28bb4a42747e096abb5a36cbdc6c
Matthew Sobieray <matt@hyfyn.com>
2020-06-14 16:53:23 -0600

    final clean up

--- server/db/sql_statements.sql
+++ server/db/sql_statements.sql
@@ -1,7 +1,6 @@
 -- SQLite
 INSERT INTO wiki (page_name, page_slug, page_content, created_at, updated_at)
-VALUES ('page2', 'page_2', 'this is content', DateTime('now'), DateTime('now'));
-
+VALUES ('First Wiki', 'first-wiki', '#this is content', DateTime('now'), DateTime('now'));
 
 UPDATE wiki 
 SET page_name = 'page3',
@@ -12,7 +11,6 @@ DELETE FROM wiki
 WHERE rowid = 3;
 
 SELECT wiki_id, page_name from wiki;
-
 
 SELECT rowid, page_name, page_slug, page_content FROM wikis WHERE rowid = 11;
 
